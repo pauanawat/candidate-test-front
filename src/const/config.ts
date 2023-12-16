8db8094d9b54a800c92c045aeba24f9b93adc858
Anawat <alonypau@gmail.com>
2023-12-17 00:57:32 +0700

    CICD - add github action, edit api url

--- src/const/config.ts
+++ src/const/config.ts
@@ -1,6 +1,6 @@
 export const config = {
   // API_URL: process.env.API_URL || '',
   // BASIC_API: process.env.BASIC_API || ''
-  API_URL: 'http://localhost:3001',
+  API_URL: 'https://candidate-test-backend-3aaa43f87169.herokuapp.com',
   BASIC_API: 'NEYtWDY4U2I0I2FQcHduSGhOaT81ZV93QyYkTVA/OnpNW1J4W3VhUDlSQGJnUjRAWmlwITYtJjhpMHYzTg=='
 }
